f2c87e40b37710dbf4b2ba0041f73642b93f335e
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-05-26 19:31:11 +0700

    home clear

---
+++ Movix/src/data/dataGendre.js
@@ -0,0 +1,45 @@
+export const gendreList = [
+    {
+        gendre: 'All'
+    },
+    {
+        gendre: 'Action'
+    },
+    {
+        gendre: 'Drama'
+    },
+    {
+        gendre: 'Romance'
+    },
+    {
+        gendre: 'Biography'
+    },
+    {
+        gendre: 'Sport'
+    },
+    {
+        gendre: 'Horror'
+    },
+    {
+        gendre: 'Adventure'
+    },
+    {
+        gendre: 'Sci-Fi'
+    },
+    {
+        gendre: 'Comedy'
+    },
+    {
+        gendre: 'Crime'
+    },
+    {
+        gendre: 'Thriller'
+    },
+    {
+        gendre: 'Animation'
+    },
+    {
+        gendre: 'Mystery'
+    },
+
+];
--- Movix/src/screen/homePage.js
+++ Movix/src/screen/homePage.js
@@ -9,6 +9,10 @@ import {
     View,
 } from 'react-native';
 import styles from "../style/homeStyle"
+import Gendre from "../componen/gendre";
+import TopThreeFilm from "../componen/topThreeFilm";
+import Populer from "../componen/populer";
+import ComingSoon from "../componen/comingSoon";
 
 const HomePage = () => {
 
@@ -17,6 +21,26 @@ const HomePage = () => {
             <ScrollView>
                 <View style={styles.margin}>
                     <Text style={styles.textTitle}>Movix</Text>
+                    <View style={styles.view20} />
+
+                    <Gendre />
+
+                    <View style={styles.view30} />
+
+                    <TopThreeFilm />
+                    <View style={styles.view40} />
+
+                    <Text style={styles.textSubTittle}>Populer</Text>
+                    <View style={styles.view20} />
+
+                    <Populer />
+
+                    <View style={styles.view30} />
+                    <Text style={styles.textSubTittle}>Coming Soon</Text>
+                    <View style={styles.view20} />
+                    <ComingSoon />
+
+                    <View style={styles.view30} />
                 </View>
             </ScrollView>
         </SafeAreaView>
