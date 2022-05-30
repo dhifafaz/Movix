af063f2c6d9b461c7ddb3b65dca990bd92ed5a98
Aulia Rahman Zulfi <dragoncimol@gmail.com>
2022-05-30 10:52:22 +0700

    ygy

--- Movix/src/componen/populer.js
+++ Movix/src/componen/populer.js
@@ -1,155 +1,164 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from 'react';
 import {
-    Image,
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    useColorScheme,
-    View,
-    FlatList,
-    Pressable,
+  Image,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  FlatList,
+  Pressable,
 } from 'react-native';
-import { ImageListTemporary } from "../data/imageFilm";
+import {ImageListTemporary} from '../data/imageFilm';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchApiListFilm } from "../redux/action";
-import { useNavigation } from '@react-navigation/native';
+import {useSelector, useDispatch} from 'react-redux';
+import {fetchApiListFilm} from '../redux/action';
+import {useNavigation} from '@react-navigation/native';
 
 const Populer = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
 
-    const dispatch = useDispatch()
-    const navigation = useNavigation()
+  const {dataListFilm, linkFoto, gendreSelect} = useSelector(
+    state => state.userReducer,
+  );
 
-    const { dataListFilm, linkFoto, gendreSelect } = useSelector(state => state.userReducer);
+  useEffect(() => {
+    dispatch(fetchApiListFilm());
+  }, []);
 
-    useEffect(() => {
-        dispatch(fetchApiListFilm())
-
-    }, []);
-
-    return (
-        <View>
-            <FlatList
-                horizontal={true}
-                data={dataListFilm}
-                renderItem={({ item, index, separators }) => {
-                    if (gendreSelect == 'All') {
-                        return (
-                            <Pressable
-                                style={{ width: 120, marginRight: 10 }}
-                                onPress={() => {
-                                    navigation.navigate('DetailPage', {
-                                        title: item.title,
-                                        rating: item.rating,
-                                        tahun_terbit: item.tahun_terbit,
-                                        genre: item.genre,
-                                        durasi: item.durasi,
-                                        description: item.description,
-                                        sutradara: item.sutradara,
-                                        image: linkFoto + item.image,
-                                    })
-                                }}
-
-                            >
-                                <Image
-                                    source={{ uri: linkFoto + item.image }}
-                                    style={{ height: 200, width: 120, borderRadius: 10 }}
-                                />
-                                <View style={styles.view10} />
-                                <Text style={styles.text} ellipsizeMode="tail" numberOfLines={1}>{item.title}</Text>
-                                <View style={styles.row}>
-                                    <MaterialCommunityIcons name="star" color={'#F8B911'} size={15} />
-                                    <Text style={styles.textReting}>{item.rating}</Text>
-                                </View>
-                            </Pressable>
-                        )
-                    } else {
-                        if (gendreSelect == item.genre) {
-                            return (
-                                <Pressable
-                                    style={{ width: 120, marginRight: 10 }}
-                                    onPress={() => {
-                                        navigation.navigate('DetailPage', {
-                                            title: item.title,
-                                            rating: item.rating,
-                                            tahun_terbit: item.tahun_terbit,
-                                            genre: item.genre,
-                                            durasi: item.durasi,
-                                            description: item.description,
-                                            sutradara: item.sutradara,
-                                            image: linkFoto + item.image,
-                                        })
-                                    }}
-
-                                >
-                                    <Image
-                                        source={{ uri: linkFoto + item.image }}
-                                        style={{ height: 200, width: 120, borderRadius: 10 }}
-                                    />
-                                    <View style={styles.view10} />
-                                    <Text style={styles.text} ellipsizeMode="tail" numberOfLines={1}>{item.title}</Text>
-                                    <View style={styles.row}>
-                                        <MaterialCommunityIcons name="star" color={'#F8B911'} size={15} />
-                                        <Text style={styles.textReting}>{item.rating}</Text>
-                                    </View>
-                                </Pressable>
-                            )
-                        }
-                    }
-
-                }}
-                keyExtractor={(item) => item.gendre}
-
-            />
-        </View>
-    )
-
-}
+  return (
+    <View>
+      <FlatList
+        horizontal={true}
+        data={dataListFilm}
+        renderItem={({item, index, separators}) => {
+          if (gendreSelect == 'All') {
+            return (
+              <Pressable
+                style={{width: 120, marginRight: 10}}
+                onPress={() => {
+                  navigation.navigate('DetailPage', {
+                    title: item.title,
+                    rating: item.rating,
+                    tahun_terbit: item.tahun_terbit,
+                    genre: item.genre,
+                    durasi: item.durasi,
+                    description: item.description,
+                    sutradara: item.sutradara,
+                    image: linkFoto + item.image,
+                  });
+                }}>
+                <Image
+                  source={{uri: linkFoto + item.image}}
+                  style={{height: 200, width: 120, borderRadius: 10}}
+                />
+                <View style={styles.view10} />
+                <Text
+                  style={styles.text}
+                  ellipsizeMode="tail"
+                  numberOfLines={1}>
+                  {item.title}
+                </Text>
+                <View style={styles.row}>
+                  <MaterialCommunityIcons
+                    name="star"
+                    color={'#F8B911'}
+                    size={15}
+                  />
+                  <Text style={styles.textReting}>{item.rating}</Text>
+                </View>
+              </Pressable>
+            );
+          } else {
+            if (gendreSelect == item.genre) {
+              return (
+                <Pressable
+                  style={{width: 120, marginRight: 10}}
+                  onPress={() => {
+                    navigation.navigate('DetailPage', {
+                      title: item.title,
+                      rating: item.rating,
+                      tahun_terbit: item.tahun_terbit,
+                      genre: item.genre,
+                      durasi: item.durasi,
+                      description: item.description,
+                      sutradara: item.sutradara,
+                      image: linkFoto + item.image,
+                    });
+                  }}>
+                  <Image
+                    source={{uri: linkFoto + item.image}}
+                    style={{height: 200, width: 120, borderRadius: 10}}
+                  />
+                  <View style={styles.view10} />
+                  <Text
+                    style={styles.text}
+                    ellipsizeMode="tail"
+                    numberOfLines={1}>
+                    {item.title}
+                  </Text>
+                  <View style={styles.row}>
+                    <MaterialCommunityIcons
+                      name="star"
+                      color={'#F8B911'}
+                      size={15}
+                    />
+                    <Text style={styles.textReting}>{item.rating}</Text>
+                  </View>
+                </Pressable>
+              );
+            }
+          }
+        }}
+        keyExtractor={item => item.gendre}
+      />
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-    row: {
-        flexDirection: "row"
-    },
-    view10: {
-        height: 10,
-    },
-    view30: {
-        height: 30,
-    },
-    view40: {
-        height: 40,
-    },
-    touch: {
-        width: 100,
-        height: 30,
-        backgroundColor: '#4A6495',
-        borderRadius: 10,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginRight: 10,
-    },
-    touch1: {
-        width: 100,
-        height: 30,
-        backgroundColor: '#0054FE',
-        borderRadius: 10,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginRight: 10,
-    },
-    text: {
-        fontSize: 16,
-        fontFamily: 'Ubuntu-Medium',
-        color: '#FFFFFF',
-    },
-    textReting: {
-        fontSize: 12,
-        fontFamily: 'Ubuntu-Medium',
-        color: '#FFFFFF',
-        marginLeft: 5,
-    }
-
-})
-
+  row: {
+    flexDirection: 'row',
+  },
+  view10: {
+    height: 10,
+  },
+  view30: {
+    height: 30,
+  },
+  view40: {
+    height: 40,
+  },
+  touch: {
+    width: 100,
+    height: 30,
+    backgroundColor: '#4A6495',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10,
+  },
+  touch1: {
+    width: 100,
+    height: 30,
+    backgroundColor: '#0054FE',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10,
+  },
+  text: {
+    fontSize: 16,
+    fontFamily: 'Ubuntu-Medium',
+    color: '#FFFFFF',
+  },
+  textReting: {
+    fontSize: 12,
+    fontFamily: 'Ubuntu-Medium',
+    color: '#FFFFFF',
+    marginLeft: 5,
+  },
+});
 
 export default Populer;
--- Movix/src/screen/profilPage.js
+++ Movix/src/screen/profilPage.js
@@ -8,54 +8,64 @@ import {
   useColorScheme,
   View,
   Image,
+  Pressable,
+  Linking,
 } from 'react-native';
 import styles from '../style/profilSyle';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const ProfilPage = ({ navigation }) => {
+const ProfilPage = ({navigation}) => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
-        <Image
-          style={styles.picture}
-          source={require('../image/singa.jpg')}
-        />
+        <Image style={styles.picture} source={require('../image/singa.jpg')} />
         <Text style={styles.nameText}> FADI AUMAY</Text>
       </View>
-      <View style={styles.thirdTab}>
+      <Pressable
+        style={styles.thirdTab}
+        // onPress={Linking.openURL('https://google.com')}
+      >
         <MaterialCommunityIcons
           name="instagram"
           size={32}
-          style={{ color: 'white', marginTop: 8, marginLeft: 10 }}
+          style={{color: 'white', marginTop: 8, marginLeft: 10}}
         />
         <Text style={styles.subJud}> afarras27 </Text>
-      </View>
-      <View style={styles.thirdTab}>
+      </Pressable>
+      <Pressable
+        style={styles.thirdTab}
+        // onPress={Linking.openURL('https://google.com')}
+      >
         <MaterialCommunityIcons
           name="instagram"
           size={32}
-          style={{ color: 'white', marginTop: 8, marginLeft: 10 }}
+          style={{color: 'white', marginTop: 8, marginLeft: 10}}
         />
         <Text style={styles.subJud}> dhifafaz </Text>
-      </View>
-      <View style={styles.thirdTab}>
+      </Pressable>
+      <Pressable
+        style={styles.thirdTab}
+        // onPress={Linking.openURL('https://google.com')}
+      >
         <MaterialCommunityIcons
           name="instagram"
           size={32}
-          style={{ color: 'white', marginTop: 8, marginLeft: 10 }}
+          style={{color: 'white', marginTop: 8, marginLeft: 10}}
         />
         <Text style={styles.subJud}> mayang.h.a </Text>
-      </View>
-      <View style={styles.thirdTab}>
+      </Pressable>
+      <Pressable
+        style={styles.thirdTab}
+        // onPress={Linking.openURL('https://google.com')}
+      >
         <MaterialCommunityIcons
           name="instagram"
           size={32}
-          style={{ color: 'white', marginTop: 8, marginLeft: 10 }}
+          style={{color: 'white', marginTop: 8, marginLeft: 10}}
         />
         <Text style={styles.subJud}> auliarahmanz </Text>
-      </View>
-
+      </Pressable>
     </SafeAreaView>
   );
 };
